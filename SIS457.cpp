533c57d9f5ded92f810a7fc285e8137054268340
unknown <www.hackdcjcc@hotmail.com>
2022-09-23 23:04:36 -0400

    SIS457

---
+++ SIS457.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"\t\t\t\t\t Laboratorio 1"<<endl;
+cout<<" "<<endl;
+cout<<"Estudiante: Jhon Carlos Condori Cuevas ";
+cout<<"\t CI: 13123129";
+cout<<"\t CU: 35-4733"<<endl;
+cout<<" "<<endl;
+cout<<"Ante Dios y mi concencia yo Jhon Carlos me comprometo dedicarme ser mas responsable y dar todo de mi con la materia de SIS457 para no volver a repetirla"<<endl;
+cout<<" "<<endl;
+cout<<"\t\t\t\t\t\tHola Mundo"; 
+cout<<" "<<endl;
+
+    return 0;
+}
